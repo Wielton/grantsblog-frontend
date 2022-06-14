553221979990290a932f04f6d4a75f313b4f4086
Wielton <grantwielgosz@gmail.com>
2022-06-13 22:59:05 -0600

    somewhat ready for deployment

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>Welcome to Grant's Blog About Nothing</h1>
-    
-      <input type="text" placeholder="Username" v-model="userName">
-      <textarea type="text" placeholder="Post" v-model="userPost"></textarea>
-      <button @click="createPost">CREATE</button>
-      <button @click="getPosts">GET</button>
-      <ul 
-      v-for="post in posts"
-      :key="post.id"
-      :bind="post"
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Grant's Blog About Nothing
+        </h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        class="mb-5"
+        cols="4"
+        
       >
-        <li>{{post.username}}{{post.post}}</li>
-      </ul>
-  </div>   
+      <v-card
+        center>
+        <v-text-field
+          label="Username"
+          v-model="userName"
+          ></v-text-field>
+        <v-textarea
+          outlined
+          style="width:85%"
+          type="text"
+          label="Post" 
+          v-model="userPost"
+          counter="200"></v-textarea>
+          <v-card-actions>
+            <v-btn @click="createPost">Create</v-btn>
+          </v-card-actions>
+      </v-card>
+      </v-col>
+        <!-- <v-row justify="center"> -->
+          <v-col 
+              class="mb-5"
+              cols="6"
+              >
+              <v-btn @click="getPosts">See All</v-btn>
+          <v-card
+            v-for="post in posts"
+            :key="post.id"
+            :bind="post"
+            style="background-color:#383838;border:1px solid white"
+          >
+            <v-card-title style="color:whitesmoke">{{post.username}}</v-card-title>
+            <v-card-text style="color:whitesmoke">{{post.post}}</v-card-text>
+            <v-card-actions>
+              <v-btn @click="editPost" style="background-color:green">Edit</v-btn>
+              <v-btn @click="deletePost" style="background-color:red">Delete</v-btn>
+            </v-card-actions>
+          </v-card>
+          </v-col>
+        <!-- </v-row> -->
+      
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      userName : null,
-      userPost : null,
+  import axios from 'axios'
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      userName: null,
+      userPost: null,
       posts : []
-    }
-  },
-  methods: {
+    }),
+    methods: {
     createPost(){
       axios.request({
         url : process.env.VUE_APP_API_URL+"posts",
@@ -56,9 +103,12 @@ export default {
     editPost(){
       axios.request({
         url : process.env.VUE_APP_API_URL+"posts",
-        method : "GET"
+        method : "UPDATE",
+        data : {
+          userPost : this.userPost
+        }
     }).then((response)=>{
-      this.postsArr = response.data;
+      this.posts = response.data;
       console.log('This ran')
       }).catch((error)=>{
         console.log(error)
@@ -67,19 +117,14 @@ export default {
     deletePost(){
       axios.request({
         url : process.env.VUE_APP_API_URL+"posts",
-        method : "GET"
+        method : "DELETE"
     }).then((response)=>{
-      this.postsArr = response.data;
+      this.posts = response.data;
       console.log('This ran')
       }).catch((error)=>{
         console.log(error)
       })
     },
   },
-}
+  }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-</style>
